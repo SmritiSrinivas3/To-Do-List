7ecd874fa0906b8ec791c59038b3818291504df7
Smriti Srinivas <75460096+SmritiSrinivas3@users.noreply.github.com>
2022-11-03 00:19:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-const task = document.getElementById('text-box').value
 const content_space = document.getElementById('content')
 
-function addTask(){
-    if(task.trim() == '' || task.trim() == null){
+
+function addTask() {
+    let task = document.getElementById('text-box').value
+    if (task.trim() == '' || task.trim() == null) {
         return false
-    }else{
-       let list = document.createElement('li')
-       let deleteButton = document.createElement('button')
-       let editButton = document.createElement("button");
-       list.appendChild(document.createTextNode(task))
-       list.appendChild(deleteButton);
-       list.appendChild(editButton);
-       content_space.appendChild(list)
-       
+    } else {
+        let uList = document.createElement('ul')
+        let list = document.createElement('li')
+        let deleteButton = document.createElement('button')
+        deleteButton.className = 'delete'
+        let editButton = document.createElement("button");
+        editButton.className = 'edit'
+        list.appendChild(document.createTextNode(task))
+        list.appendChild(deleteButton);
+        deleteButton.appendChild(document.createTextNode("Delete"));
+        list.appendChild(editButton);
+        editButton.appendChild(document.createTextNode("Edit"));
+        content_space.appendChild(list)
+        content_space.appendChild(uList)
     }
-} 
+}
+
+function removeItem(e){
+    e.preventDefault()
+if(e.target.classList.contains(('delete'))){
+    let li = e.target.parentNode;
+    items.removeChild(li);
+    
+}
+}
